ba42d07e680dd6be04382bd46ff6b373fb586bb9
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-11-22 19:19:01 -0600

    added grooming table and test values

--- database.sql
+++ database.sql
@@ -35,6 +35,7 @@ INSERT INTO "pet" ("name", "species" , "breed", "weight", "birthdate", "sex", "i
 VALUES ('Beau', 'dog' , 'Great Pyrenees', '175lbs', '3/3/2014', 'male', 
 'https://scontent.fmem1-2.fna.fbcdn.net/v/t31.0-8/20645088_1711365155541191_8241792617934011120_o.jpg?_nc_cat=110&ccb=2&_nc_sid=19026a&_nc_ohc=IH5MeRRktGMAX8NOw70&_nc_ht=scontent.fmem1-2.fna&oh=04ae1a38cdbc301f07cb2419eb78c561&oe=5FE21E81', 
 '1000000000', 1);
+----
 
 CREATE TABLE "food" (
 	"id" SERIAL PRIMARY KEY,
@@ -46,7 +47,7 @@ CREATE TABLE "food" (
 --Test values for food
 INSERT INTO "food" ("name" , "barcode", "pet_id") 
 VALUES ('Natures Choice' , '737257827257' , 2);
-
+----
 
 CREATE TABLE "medication" (
 	"id" SERIAL PRIMARY KEY,
@@ -58,9 +59,25 @@ CREATE TABLE "medication" (
 	"barcode" VARCHAR (120),
 	"pet_id" INT REFERENCES "pet" NOT NULL
 	);
+
 --Test values for medication
 
 INSERT INTO "medication" ("name" , "dosage" , "start_date" , "end_date" , 
 "doctor" , "barcode" , "pet_id") 
 VALUES ('Bravecto' , '250mg' , '2-2-2020' , '3-4-2020' , 
 'Dr. Magic Paws' , '8713184133625' , 2);
+----
+
+CREATE TABLE "groomer" (
+	"id" SERIAL PRIMARY KEY,
+	"groomer" VARCHAR (120),
+	"date" DATE NOT NULL,
+	"location" VARCHAR (200) NOT NULL,
+	"pet_id" INT REFERENCES "pet" NOT NULL
+	);
+
+--test values for groomer
+INSERT INTO "groomer" ("groomer" , "date" , "location" , "pet_id")
+VALUES ('Cindy' , '5-5-2020' , 'Cindy Clip and Snip' , 2);
+----
+
