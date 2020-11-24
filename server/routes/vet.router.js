0d6983b63a1f815fdcf94c2462d8e779f85f1d3e
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-11-23 20:40:29 -0600

    vet route post put delete work

--- server/routes/vet.router.js
+++ server/routes/vet.router.js
@@ -5,18 +5,43 @@ const {
   rejectUnauthenticated,
 } = require('../modules/authentication-middleware');
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-  // GET route code here
+router.get('/:id', rejectUnauthenticated, (req, res) => {
+  const queryVet = `SELECT "doctor", "reason", "date", "location" FROM "vet"
+      JOIN "pet" ON "vet".pet_id = "pet".id
+      WHERE pet_id = $1;`;
+  pool
+    .query(queryVet, [req.params.id])
+    .then((result) => {
+      res.send(result.rows);
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log('error in get', err);
+      res.sendStatus(500);
+    });
 });
 
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-  // POST route code here
+router.post('/:id', rejectUnauthenticated, (req, res) => {
+  const vet = req.body;
+  const insertVetQuery = `INSERT INTO "vet" 
+    ("doctor" , "reason" , "date", "location", "pet_id")
+    VALUES ($1, $2, $3, $4, $5);`;
+  pool
+    .query(insertVetQuery, [
+      vet.doctor,
+      vet.reason,
+      vet.date,
+      vet.location,
+      req.params.id,
+    ])
+    .then((result) => {
+      console.log(result.rows);
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log('Error in FOOD POST', error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
