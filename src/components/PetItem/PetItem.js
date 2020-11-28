76f41768ebddcb4a65359d62a19b2c52b70feef6
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-11-27 20:11:16 -0600

    pet display has a back button. Added material ui components but no idea how to use them yet

--- src/components/PetItem/PetItem.js
+++ src/components/PetItem/PetItem.js
@@ -3,20 +3,28 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import './PetItem.css';
-import { Button, Paper, Grid } from '@material-ui/core';
+import { Button, Paper, Grid, makeStyles, Avatar } from '@material-ui/core';
 
 class PetItem extends Component {
+  backToUser = (event) => {
+    this.props.history.push('/user');
+  };
+
   render() {
     return (
-      <div>
-        <img src={this.props.petItem.image} className="pet_image" />
-        <p className="pet_image">{this.props.petItem.name}</p>
-        <p className="pet_image">{this.props.petItem.breed}</p>
-        <p className="pet_image">{this.props.petItem.sex}</p>
-        <p className="pet_image">{this.props.petItem.birthdate}</p>
+      <Grid container spacing={3} alignItems="center" justify="center">
+        <Grid item>
+          <img src={this.props.petItem.image} className="pet" />
+          <p>{this.props.petItem.name}</p>
+          <p>{this.props.petItem.breed}</p>
+          <br />
+          <Button color="primary" onClick={this.backToUser}>
+            Back
+          </Button>
+        </Grid>
 
         {/* Need icons for vet, groomer, meds, food floating to the right. Grid time. */}
-      </div>
+      </Grid>
     );
   }
 }
--- src/pages/PetDetailsPage/PetDetailsPage.js
+++ src/pages/PetDetailsPage/PetDetailsPage.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import { withRouter } from 'react-router-dom';
 import PetItem from '../../components/PetItem/PetItem';
+import { Button, Paper, Grid } from '@material-ui/core';
 
 //renders a single pets details. Will have links to meds, vet, groomer, etc
 
@@ -14,9 +15,15 @@ class PetDetailsPage extends Component {
   render() {
     return (
       <div>
-        {this.props.store.petDetailReducer.map((petItem, index) => {
-          return <PetItem key={index} petItem={petItem} />;
-        })}
+        <Grid container spacing={3} alightItems="center" justify="center">
+          {this.props.store.petDetailReducer.map((petItem, index) => {
+            return (
+              <Grid item xs={12}>
+                <PetItem key={index} petItem={petItem} />
+              </Grid>
+            );
+          })}
+        </Grid>
       </div>
     );
   }
