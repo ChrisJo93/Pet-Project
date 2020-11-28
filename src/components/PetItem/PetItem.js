19530a571f542ed644f5b4d6dd3ffe38a6f3afae
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-11-28 11:21:02 -0600

    pet details now has icons associated with the 4 pet needs

--- src/components/PetItem/PetItem.js
+++ src/components/PetItem/PetItem.js
@@ -4,26 +4,45 @@ import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import './PetItem.css';
 import { Button, Paper, Grid, makeStyles, Avatar } from '@material-ui/core';
+import {
+  AccessAlarm,
+  Fastfood,
+  Bathtub,
+  LocalHospital,
+  LocalPharmacy,
+} from '@material-ui/icons';
 
 class PetItem extends Component {
   backToUser = (event) => {
     this.props.history.push('/user');
   };
 
+  test = (event) => {
+    console.log('This will need to be a gentle alert soon.');
+  };
+
   render() {
     return (
-      <Grid container spacing={3} alignItems="center" justify="center">
+      <Grid container spacing={10} alignItems="center" justify="center">
         <Grid item>
           <img src={this.props.petItem.image} className="pet" />
           <p>{this.props.petItem.name}</p>
           <p>{this.props.petItem.breed}</p>
           <br />
+          <Grid container alignItems="center" justify="space-between">
+            {/* Each icon needs an onMouseEnter with title floating above it.
+              Each icon needs to link to respective page. */}
+            <Fastfood title="food" />
+            <Bathtub title="groomer" onMouseEnter={this.test} />
+            <LocalHospital title="Vet" />
+            <LocalPharmacy title="Medicine" />
+          </Grid>
+          <Grid item />
           <Button color="primary" onClick={this.backToUser}>
             Back
           </Button>
+          <Grid />
         </Grid>
-
-        {/* Need icons for vet, groomer, meds, food floating to the right. Grid time. */}
       </Grid>
     );
   }
