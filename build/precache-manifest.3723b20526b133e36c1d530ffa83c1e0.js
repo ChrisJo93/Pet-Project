90ca430fb236cf8e06dc681ba9cc457e66aaccc9
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-12-07 16:25:50 -0600

    production build for client

---
+++ build/precache-manifest.3723b20526b133e36c1d530ffa83c1e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ae6671f103e4682ae9a4612664a7e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29167ab1ee2723256816",
+    "url": "/static/css/main.f4166c6a.chunk.css"
+  },
+  {
+    "revision": "f5b28e5d5719c6837c7b",
+    "url": "/static/js/2.7e099902.chunk.js"
+  },
+  {
+    "revision": "1b971492c3a12482004b99f08fbbd0ff",
+    "url": "/static/js/2.7e099902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29167ab1ee2723256816",
+    "url": "/static/js/main.a48fd32c.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  }
+]);
